pyinmana/system76-driver

commit 345cd929d042f6fd8b444d03aab0c3805db87298

Load USB audio S/PDIF output device
Fix S/PDIF audio on thelio-mega-r1 and thelio-major-r2 by explicity
loading a PulseAudio module for it. The S/PDIF device on the mega-r1
using index 1, while on the major-r2 it uses an index of 3.

system76driver/actions.py
@@ -33,6 +33,7 @@ import logging
 
 from . import get_datafile
 from .mockable import SubProcess
+from .model import determine_model_new
 
 
 log = logging.getLogger()
@@ -1320,7 +1321,37 @@ class usb_audio_load_microphone(Action):
         return _('Load USB audio microphone device')
 
     def get_isneeded(self):
-        return self.read().splitlines()[-1] != self.value
+        return self.value not in self.read().splitlines()
+
+    def iter_lines(self):
+        content = self.read_and_backup()
+        for line in content.splitlines():
+            if line != self.value:
+                yield line
+        yield self.value
+
+    def perform(self):
+        content = '\n'.join(self.iter_lines())
+        self.atomic_write(content)
+
+class usb_audio_load_spdif(Action):
+    def __init__(self, etcdir='/etc'):
+        self.filename = path.join(etcdir, 'pulse', 'default.pa')
+
+        sink = '''load-module module-alsa-sink device=hw:CARD=Audio,DEV={} sink_properties="device.description='S/PDIF'"\n'''
+        if determine_model_new() == 'thelio-major-r2':
+            self.value = sink.format('3')
+        else:
+            self.value = sink.format('1')
+
+    def read(self):
+        return open(self.filename, 'r').read()
+
+    def describe(self):
+        return _('Load USB audio S/PDIF device')
+
+    def get_isneeded(self):
+        return self.value not in self.read().splitlines()
 
     def iter_lines(self):
         content = self.read_and_backup()

system76driver/products.py
@@ -864,6 +864,7 @@ PRODUCTS = {
         'drivers': [
             actions.usb_audio_ignore_ctl_error,
             actions.usb_audio_load_microphone,
+            actions.usb_audio_load_spdif,
         ],
     },
     'thelio-massive-b1': {
@@ -879,6 +880,7 @@ PRODUCTS = {
         'drivers': [
             actions.usb_audio_ignore_ctl_error,
             actions.usb_audio_load_microphone,
+            actions.usb_audio_load_spdif,
         ],
     },
 

commit 7893bff93009f3ca8f3cfcb048a25079f034e775

actions: Fix string comparison involving newline
`splitlines()` strips the line endings, causing `get_isneeded()` to
always return true. This caused the setting to always be appended to
the PulseAudio config file when system76driver was run.

Fixes: 345cd929d042f ("Load USB audio S/PDIF output device")

system76driver/actions.py
@@ -1338,7 +1338,7 @@ class usb_audio_load_spdif(Action):
     def __init__(self, etcdir='/etc'):
         self.filename = path.join(etcdir, 'pulse', 'default.pa')
 
-        sink = '''load-module module-alsa-sink device=hw:CARD=Audio,DEV={} sink_properties="device.description='S/PDIF'"\n'''
+        sink = 'load-module module-alsa-sink device=hw:CARD=Audio,DEV={} sink_properties="device.description=\'S/PDIF\'"'
         if determine_model_new() == 'thelio-major-r2':
             self.value = sink.format('3')
         else: