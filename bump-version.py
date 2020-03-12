pyinmana/system76-driver

commit 8afa9c9256275eb369c580dff01e581ee56b646c

update d/control, d/changelog and switches platform to distro to work on 20.04

bump-version.py
@@ -34,7 +34,7 @@ from system76driver import __version__
 from system76driver.tests.helpers import TempDir
 
 
-DISTROS = ('trusty', 'xenial', 'yakkety', 'zesty', 'artful', 'bionic', 'cosmic', 'disco', 'eoan')
+DISTROS = ('trusty', 'xenial', 'yakkety', 'zesty', 'artful', 'bionic', 'cosmic', 'disco', 'eoan', 'focal')
 ALPHA = '~~alpha'
 
 TREE = path.dirname(path.abspath(__file__))

debian/control
@@ -4,6 +4,7 @@ Priority: extra
 Maintainer: System76, Inc. <dev@system76.com>
 Build-Depends: debhelper (>= 9),
     dh-systemd,
+    dh-python,
     gir1.2-gtk-3.0,
     gir1.2-notify-0.7,
     pyflakes3,
@@ -11,6 +12,7 @@ Build-Depends: debhelper (>= 9),
     python3-dbus,
     python3-evdev,
     python3-gi,
+    python3-distro,
     python3-systemd,
     xbacklight
 Standards-Version: 3.9.6
@@ -31,6 +33,7 @@ Depends: ${python3:Depends}, ${misc:Depends},
     python3-dbus,
     python3-evdev,
     python3-gi,
+    python3-distro,
     python3-systemd,
     system76-acpi-dkms,
     system76-dkms,

make-release.py
@@ -34,7 +34,7 @@ from system76driver import __version__
 from system76driver.tests.helpers import TempDir
 
 
-DISTROS = ('trusty', 'xenial', 'yakkety', 'zesty', 'artful', 'bionic', 'cosmic', 'disco', 'eoan')
+DISTROS = ('trusty', 'xenial', 'yakkety', 'zesty', 'artful', 'bionic', 'cosmic', 'disco', 'eoan', 'focal')
 PPA = 'ppa:system76-dev/pre-stable'
 ALPHA = '~~alpha'
 

system76-driver
@@ -24,7 +24,7 @@ import os
 import sys
 import time
 import logging
-import platform
+import distro
 
 import system76driver
 from system76driver.model import determine_model_new
@@ -59,8 +59,8 @@ product = PRODUCTS.get(args.model)
 
 log.info('** Process start at monotonic time %r', time.monotonic())
 log.info('system76driver.__version__: %r', system76driver.__version__)
-log.info('OS: %r', platform.dist())
-log.info('kernel: %r', platform.uname().release)
+log.info('OS: %r', distro.name())
+log.info('kernel: %r', distro.os.uname().release)
 log.info('model: %r', args.model)
 
 ui = UI(args.model, product, args)

system76driver/__init__.py
@@ -25,7 +25,7 @@ from os import path
 import logging
 
 
-__version__ = '19.10.2'
+__version__ = '20.04.1'
 
 datadir = path.join(path.dirname(path.abspath(__file__)), 'data')
 log = logging.getLogger(__name__)

system76driver/gtk.py
@@ -21,7 +21,7 @@
 Gtk UI.
 """
 
-import platform
+import distro
 import threading
 from gettext import gettext as _
 
@@ -54,7 +54,7 @@ class UI:
         self.details = self.builder.get_object('detailsText')
         self.builder.get_object('sysModel').set_text(model)
         self.builder.get_object('ubuntuVersion').set_text(
-            '{} {} ({})'.format(*platform.dist())
+            '{} {} ({})'.format(*distro.linux_distribution())
         )
         self.builder.get_object('driverVersion').set_text(__version__)
 

system76driver/util.py
@@ -25,7 +25,7 @@ import os
 from os import path
 import shutil
 import tempfile
-import platform
+import distro
 
 from .model import determine_model
 from .mockable import SubProcess
@@ -34,8 +34,8 @@ from .mockable import SubProcess
 def dump_logs(base):
     fp = open(path.join(base, 'systeminfo.txt'), 'x')
     fp.write('System76 Model: {}\n'.format(determine_model()))
-    fp.write('OS Version: {}\n'.format(', '.join(platform.dist())))
-    fp.write('Kernel Version: {}\n'.format(platform.uname().release))
+    fp.write('OS Version: {}\n'.format(', '.join(distro.name())))
+    fp.write('Kernel Version: {}\n'.format(distro.os.uname().release))
 
     fp = open(path.join(base, 'dmidecode'), 'xb')
     SubProcess.check_call(['dmidecode'], stdout=fp)

commit cdbad59c99a15c554b4ba7a3ae1832b3bb6c7401

bump-version: Allow any urgency level
`bump-version.py` expects an urgency level of "low". 8afa9c9256275 broke
this by using an urgency level of "medium". Relax this condition by
allowing any valid urgency level.

Fixes: 8afa9c9256275 ("update d/control, d/changelog and switches platform to distro to work on 20.04")

bump-version.py
@@ -71,7 +71,7 @@ def parse_version_line(line):
     if ALPHA in line:
         raise ValueError('{!r} in current version:\n{!r}'.format(ALPHA, line))
     m = re.match(
-        '^system76-driver \(([\.0-9]+)\) ([a-z]+); urgency=low$', line
+        '^system76-driver \(([\.0-9]+)\) ([a-z]+); urgency=(low|medium|high|emergency|critical)$', line
     )
     if m is None:
         raise ValueError('bad version line[0]:\n{!r}'.format(line))